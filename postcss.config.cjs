b94c63693eca0b64213dba52e8ec9084a98ac34a
m-housni <mohamedhousni1000k@gmail.com>
2025-03-06 22:33:20 +0000

    add Tailwind CSS configuration

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+// filepath: c:\Users\hp\Desktop\themed-react-components-using-abstract-factory-pattern\postcss.config.cjs
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss7-compat': {},
+    autoprefixer: {},
+  },
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+// filepath: c:\Users\hp\Desktop\themed-react-components-using-abstract-factory-pattern\src\main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css'; // Ensure this import is present
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
