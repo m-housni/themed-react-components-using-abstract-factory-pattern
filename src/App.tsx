0b1d599afc0b721a0ff469846f260cb4cd5fd206
m-housni <mohamedhousni1000k@gmail.com>
2025-03-06 23:04:35 +0000

    implement theme toggling in App component with ThemeContext and ThemedButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <>
-      <div>Hello React!</div>  
-    </>
-  )
-}
+import React from "react";
+import ThemeContext from "./context/ThemeContext";
+import DarkThemeFactory from "./factories/DarkThemeFactory";
+import LightThemeFactory from "./factories/LightThemeFactory";
+import ThemedButton from "./components/Button/ThemedButton";
 
-export default App
+
+
+const App: React.FC = () => {
+  const [isDark, setIsDark] = React.useState(false);
+  return (
+    <ThemeContext.Provider value={isDark ? DarkThemeFactory : LightThemeFactory}>
+      <div className={`min-h-screen p-5 ${isDark ? "bg-gray-900 text-white" : "bg-gray-100 text-black"}`}>
+        <button onClick={() => setIsDark(!isDark)} className="mb-4 px-4 py-2 bg-blue-500 text-white rounded">
+          Toggle Theme
+        </button>
+        <ThemedButton label="Btn 1" onClick={() => alert("Button 1 Clicked")} />
+        <ThemedButton label="Btn 2" onClick={() => alert("Button 2 Clicked")} />
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export default App;
+
