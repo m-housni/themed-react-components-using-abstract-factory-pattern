6c702c721581e513c461e3d0f7b59c9861133706
m-housni <mohamedhousni1000k@gmail.com>
2025-03-06 23:02:39 +0000

    add margin to DarkButton and LightButton components

--- src/components/Button/DarkButton.tsx
+++ src/components/Button/DarkButton.tsx
@@ -1,7 +1,7 @@
 import ButtonProps from "../../interfaces/ButtonProps";
 
 const DarkButton: React.FC<ButtonProps> = ({ label, onClick }) => (
-  <button className="bg-black text-white px-4 py-2 rounded" onClick={onClick}>
+  <button className="bg-black text-white px-4 py-2 rounded ml-1" onClick={onClick}>
     {label}
   </button>
 );
--- src/components/Button/LightButton.tsx
+++ src/components/Button/LightButton.tsx
@@ -2,7 +2,7 @@ import ButtonProps from "../../interfaces/ButtonProps";
 
 const LightButton: React.FC<ButtonProps> = ({ label, onClick }) => (
   <button
-    className="bg-white text-black border border-gray-300 px-4 py-2 rounded"
+    className="bg-white text-black border border-gray-300 px-4 py-2 rounded ml-1"
     onClick={onClick}
   >
     {label}
